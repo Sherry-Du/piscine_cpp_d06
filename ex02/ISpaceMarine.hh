a6c43cb1fe592422b104978b5dbd2e8021c2994f
sherry-du <726845321@qq.com>
2018-07-09 20:24:44 +0800

    update

--- ex02/ISpaceMarine.hh
+++ ex02/ISpaceMarine.hh
@@ -1,15 +1,5 @@
-/*
- ** ISpaceMarine.hh for ex02 in /home/fest/Epitech/Piscine_tek2/piscine_cpp_d10-2016-ginter_m/ex02
- **
- ** Made by maxime ginters
- ** Login  <ginter_m@epitech.eu>
- **
- ** Started on  Fri Jan 18 16:55:34 2013 maxime ginters
- ** Last update Fri Jan 18 16:55:37 2013 maxime ginters
- */
-
 #ifndef ISPACEMARINE_H_
-# define ISPACEMARINE_H_
+#define ISPACEMARINE_H_
 
 class ISpaceMarine
 {
--- ex02/ISquad.hh
+++ ex02/ISquad.hh
@@ -1,15 +1,5 @@
-/*
-** ISquad.hh for ex02 in /home/fest/Epitech/Piscine_tek2/piscine_cpp_d10-2016-ginter_m/ex02
-**
-** Made by maxime ginters
-** Login  <ginter_m@epitech.eu>
-**
-** Started on  Fri Jan 18 16:11:21 2013 maxime ginters
-** Last update Fri Jan 18 17:32:58 2013 maxime ginters
-*/
-
 #ifndef ISQUAD_H_
-# define ISQUAD_H_
+#define ISQUAD_H_
 
 class ISpaceMarine;
 
